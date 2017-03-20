0ad20f06f0f8d58d592e2d726b0755b538bdd6d6
Stephen Cavaliere <stephencavaliere@gmail.com>
2017-03-20 02:50:48 -0400

    chore: add docker

--- src/repositories/firebase/firebase.context.ts
+++ src/repositories/firebase/firebase.context.ts
@@ -1,9 +1,21 @@
 import * as admin from 'firebase-admin';
-const firebaseServiceAccount = require('../../../firebase.json');
+
+const firebaseConfig = {
+  type: 'service_account',
+  project_id: process.env.FIREBASE_PROJECT_ID || '',
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID || '',
+  private_key: process.env.FIREBASE_PRIVATE_KEY || '',
+  client_email: process.env.FIREBASE_CLIENT_EMAIL || '',
+  client_id: process.env.FIREBASE_CLIENT_ID || '',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://accounts.google.com/o/oauth2/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL || '',
+};
 
 function createFirebaseContext() {
   admin.initializeApp({
-    credential: admin.credential.cert(firebaseServiceAccount),
+    credential: admin.credential.cert(firebaseConfig),
     databaseURL: process.env.FIREBASE_DATABASE_URL
   });
 
